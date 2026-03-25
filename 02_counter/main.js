935578200c15c6ba55316dac8be334644dd31d39
jgj1020 <jang08102jgj1620@gmail.com>
2026-03-25 10:09:35 +0900

    counter +, +2, -, reset

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,18 +1,41 @@
 let number = 0;
 
 // h1에 있는 숫자 요소를 가져오자
-//const resultH1 = document.getElementsByTagName('h1')[0];
 const resultH1 = document.getElementById('result');
-// button에 있는 + 요소를 가져오자
-//const plusButton = document.getElementsByTagName('button')[0];
-const plusButton = document.querySelector('.plus');
+// const resultH1 = document.getElementsByTagName('h1')[0];
 
-// plusButton.addEventListener("click", () =>{
+// button에 있는 + 요소를 가져오자
+const plusButton = document.querySelector('.plus');
+// const plusButton = document.getElementsByTagName('button')[0];
+
+// 이벤트 방식 (버튼 클릭 시 실행)
+// plusButton.addEventListener("click", () => {
 //     number++;
 //     resultH1.innerText = number;
 // });
-plusButton.addEventListener("click", () =>{
+plusButton.onclick = plus;
+
+// 함수 정의
+const plus = () => {
     number++;
     resultH1.innerText = number;
-});
+}
 
+const minus = () => {
+    number--;
+    resultH1.innerText = number;
+}
+
+const plus2 = () => {
+    number += 2;
+    resultH1.innerText = number;
+}
+const plus1 = () => {
+    number ;
+    resultH1.innerText = number;
+}
+
+const reset = () => {
+    number = 0;
+    resultH1.innerText = number;
+}
