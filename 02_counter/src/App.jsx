a19f5e24e2229e0ed183fa0577e13242e667cdd0
jgj1020 <jang08102jgj1620@gmail.com>
2026-04-03 09:40:30 +0900

    CounterApp +2, -, reset

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -4,7 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
+function 
+App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -18,7 +19,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
--- 02_counter/src/CounterApp.jsx
+++ 02_counter/src/CounterApp.jsx
@@ -1,19 +1,28 @@
-import
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+// import './App.css'
 
 function CounterApp() {
-    //const: 현재 또는 잊ㄴ 상태 값; setCount(): 상태값 변경 함수 
-    const [counter, setCount] = useState(0)
+  //count: 현재 또는 이전 상태 값; setCount(): 상태값 변경 함수
+  const [count, setCount] = useState(0)
 
-    return (
-        <>
-        <h1>{count}</h1>
-        <button onClick={() => setCount((count) => count + 1)}>+</button>
-        {/*<button 
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count + 2)}>+2</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}
-        >
-        Count is {counter}
-        </button> */}
-        </>
+      >
+        Count is {count}
+      </button> */}
+    </>
   )
 }
+
+export default CounterApp
