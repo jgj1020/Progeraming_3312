24a0557fe203c34111c35d7830ecac238a21ebcb
jgj1020 <jang08102jgj1620@gmail.com>
2026-04-22 09:10:51 +0900

    TodoListApp show todos

--- 02_counter/src/components/TodoList.jsx
+++ 02_counter/src/components/TodoList.jsx
@@ -1,11 +1,18 @@
 import TodoItem from './TodoItem.jsx';
 import TodoItemEmpty from './TodoItemEmpty.jsx';
 
-export default function TodoList() {
+export default function TodoList({todos}) {
+
     return (
          <ul className="todo__list">
-               <TodoItemEmpty/>
-                <TodoItem/>
+            {/* todos에 값이 없으면, todoItemEmpty */}
+               {todos.length === 0 && <TodoItemEmpty/>}
+               {/* todos에 값이 있으면, TodoItem에 todo를 넣자 */}
+                {todos.length > 0 &&
+                todos.map((todo) =>
+                    <TodoItem todo={todo} />
+                    )
+                }
         </ul>
     )
 }
